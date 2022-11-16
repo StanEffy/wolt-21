f2bfbd29e175b9ed0c6c913982aa89c4e6c94307
Stan Effy <holydonk@gmail.com>
2022-11-16 22:16:03 +0200

    add prettier

--- src/components/RestaurantCard/RestaurantCard.tsx
+++ src/components/RestaurantCard/RestaurantCard.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React from "react";
+import { decode } from "blurhash";
 
 interface Props {
-    blurhash: string, launch_date: string, location: [number], name: string, online: boolean, popularity: number
+  blurhash: string;
+  launch_date: string;
+  location: [number];
+  name: string;
+  online: boolean;
+  popularity: number;
 }
 
-const RestaurantCard:React.FC<Props> = ({blurhash, launch_date, location, name, online, popularity}) => {
-    return (
-        <div>
-            
-        </div>
-    );
+const RestaurantCard: React.FC<Props> = ({
+  blurhash,
+  launch_date,
+  location,
+  name,
+  online,
+  popularity,
+}) => {
+  const pixels = decode("LEHV6nWB2yk8pyo0adR*.7kCMdnj", 32, 32);
+
+  return <div></div>;
 };
 
 export default RestaurantCard;
--- src/components/RestaurantCarousel/RestaurantCarousel.tsx
+++ src/components/RestaurantCarousel/RestaurantCarousel.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const RestaurantCarousel = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div></div>;
 };
 
 export default RestaurantCarousel;
