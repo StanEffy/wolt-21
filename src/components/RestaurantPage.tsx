2750751ea58b13088f12ad7b43447b4fc058d375
Stan Effy <holydonk@gmail.com>
2022-11-16 21:52:51 +0200

    create RestaurantCard

---
+++ src/components/RestaurantPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+    blurhash: string, launch_date: string, location: [number], name: string, online: boolean, popularity: number
+}
+
+const RestaurantPage:React.FC<Props> = ({blurhash, launch_date, location, name, online, popularity}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RestaurantPage;
