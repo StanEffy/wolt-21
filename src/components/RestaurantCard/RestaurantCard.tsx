a5bd96f87b3a8167cbbc7c5ed058d96cf2710c9b
Stan Effy <holydonk@gmail.com>
2022-11-18 20:16:46 +0200

    card media resize

--- src/components/RestaurantCard/RestaurantCard.tsx
+++ src/components/RestaurantCard/RestaurantCard.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Blurhash } from "react-blurhash";
 import { IRestaurant } from "../../../types";
 import styled from "styled-components";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 const Card = styled.div`
   max-width: ${(props) => props.theme.tabletScreen};
   border: 1px solid ${(props) => props.theme.colors.primary};
+
+  @media (max-width: ${(props) => props.theme.tabletScreen}) {
+    min-width: 320px;
+  }
 `;
 
 const RestaurantCard: React.FC<IRestaurant> = ({
@@ -16,12 +21,39 @@ const RestaurantCard: React.FC<IRestaurant> = ({
   online,
   popularity,
 }) => {
+  const { width } = useWindowDimensions();
+  const [size, setSize] = useState(210);
+
+  useEffect(() => {
+    if (width < 480) {
+      if (size !== 320) {
+        setSize(320);
+      }
+    } else if (width < 768) {
+      if (size !== 210) {
+        setSize(210);
+      }
+    } else if (width < 1024) {
+      if (size !== 180) {
+        setSize(180);
+      }
+    } else if (width < 1200) {
+      if (size !== 200) {
+        setSize(200);
+      }
+    } else if (width > 1200) {
+      if (size !== 230) {
+        setSize(230);
+      }
+    }
+  }, [width]);
+
   return (
-    <Card>
+    <Card style={{ width: `${size}px` }}>
       <Blurhash
         hash={blurhash}
-        width={480}
-        height={270}
+        width={size}
+        height={Math.floor((size / 16) * 9)}
         resolutionX={32}
         resolutionY={32}
         punch={1}
